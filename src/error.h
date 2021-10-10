51eb6b4db3a9a81d0a986a70c8e7ad9045c07e3c
Josef Susík <62243819+JosephSusik@users.noreply.github.com>
2021-10-10 22:38:39 +0200

    minor changes in error.h

--- src/error.h
+++ src/error.h
@@ -18,7 +18,7 @@
 		
 	#define SCANNER_ERR 1	// Faulty lexem of IFJ21
 	#define PARSER_ERR 2	// Faulty syntax of IFJ21
-	
+
 	#define UNDEFINED_VAR_ERR 3	// Undefined function/var etc.
 	#define DATA_TYPE_ERR 4	// Error while declaring data type
 	#define PARAM_ERR 5	// Wrong number of paramters or return types
